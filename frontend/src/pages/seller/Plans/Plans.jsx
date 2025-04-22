36d9fe345d57277f27fa3fe261eb1e3ebc77b45a
bilal <bilaldeveloper900@gmail.com>
2025-04-22 00:40:18 -0700

    plan button

--- frontend/src/pages/seller/Plans/Plans.jsx
+++ frontend/src/pages/seller/Plans/Plans.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import tick from '../../../assets/images/tick.svg'
+import { Link } from 'react-router';
 
 const plansData = [
     {
@@ -65,8 +66,10 @@ const Plans = () => {
                             </th>
                             {plansData.map((plan, idx) => (
                                 <th key={idx} className="px-4 py-3 border border-[#E6E9F5]">
-                                    <div className="text-xl font-semibold">{plan.price}</div>
-                                    <div className="text-gray-500">{plan.billing}</div>
+                                    <div className='flex items-center justify-center'>
+                                        <div className="text-[24px] sm:text-[32px] font-bold">{plan.price}</div>
+                                        <div className="text-gray-500 text-sm mt-2 ml-[5px]">{plan.billing}</div>
+                                    </div>
                                     <button
                                         className={`mt-2 px-4 py-2 text-white rounded w-full ${idx === 0 ? 'bg-gray-900' : 'bg-gray-400'
                                             }`}
@@ -103,6 +106,16 @@ const Plans = () => {
                         ))}
                     </tbody>
                 </table>
+
+            </div>
+
+            <div className='mt-6'>
+                <Link
+                    to="/sell"
+                    className="w-[200px] flex justify-center items-center ml-auto  px-4 py-2 mt-8 text-center bg-[#DF0805] text-white font-medium rounded-lg"
+                >
+                    Proceed
+                </Link>
             </div>
         </div>
     )

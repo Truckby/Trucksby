66fcb898d0029671b5d31e4194f49bf96fdcb3ec
Shoaib Farooka <shoaibfarooka@gmail.com>
2025-05-05 02:32:06 +0500

    loader fixed

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,22 +1,11 @@
-import React from 'react'
 import './Loader.css';
-import { Grid } from 'react-loader-spinner'
 
-function Loader() {
+const Loader = () => {
   return (
     <div className='loader-parent'>
-      <Grid
-        visible={true}
-        height="80"
-        width="80"
-        color="#DF0805"
-        ariaLabel="grid-loading"
-        radius="12.5"
-        wrapperStyle={{}}
-        wrapperClass="grid-wrapper"
-      />
+      <div className='loader'></div>
     </div>
   )
-}
+};
 
 export default Loader;
